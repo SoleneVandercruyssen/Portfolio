17b006cf2e0c326809921d6d28bc4aef05389c2a
Solène vandercruyssen <solenevandercruyssen2@gmail.com>
2025-03-24 16:25:38 +0100

    docs: modif file script.js, contact.css

--- Portfolio/Pages/javascript/script.js
+++ Portfolio/Pages/javascript/script.js
@@ -2,10 +2,29 @@
 
 const nom = document.getElementById("name");
 const nom2 = /^[a-zA-Z]*$/;
-nom.addEventListener("input", champname);
+nom.addEventListener("change", champname);
 
 function champname() {
     if (nom = nom2) {
-        nom.style.backgroundColor = "white";
+        nom.style.backgroundColor = "black";
+    }
+    else{
+        nom.style.backgroundColor = "red"
     }
 }
+console.log(nom);
+
+const phone = document.getElementById("phone");
+const phone2 =  /^[0-9]*$/;
+phone.addEventListener("change", champphone);
+
+function champphone() {
+    if (phone = phone2) {
+        nom.style.backgroundColor = "black";
+    }
+    else{
+        nom.style.backgroundColor = "red"
+    }
+}
+
+console.log(phone);
