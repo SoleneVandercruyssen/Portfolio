bd778cd335d5a76893317a3d171eb8cd0cf9ced4
Solène vandercruyssen <solenevandercruyssen2@gmail.com>
2025-04-08 15:33:29 +0200

    docs: résolution pb popup

--- Portfolio/Pages/javascript/portfolio.js
+++ Portfolio/Pages/javascript/portfolio.js
@@ -1,32 +1,25 @@
 "use strict";
 
-const knowmore1 = document.querySelector("#btn1")    //En savoir +
-const knowmore2 = document.querySelector("#btn2")    //En savoir +
-const knowmore3 = document.querySelector("#btn3")    //En savoir +
-const knowmore4 = document.querySelector("#btn4")    //En savoir +
-const knowmore5 = document.querySelector("#btn5")    //En savoir +
-const modale = document.querySelector(".modale")
-const fermer = document.getElementById("ferme")     //Bouton Fermer
-
-// Lorsqu'on appuie sur le bouton "En savoir +" la popup s'affiche.
-knowmore1.addEventListener("click", ()=>{
-    if(knowmore1.checked){
-        modale.style.display = "block"
-    }
-})
-
-// Appuyer sur le bouton "Fermer" cela fermera la popup
-fermer.addEventListener("click", ()=>{
-    if (fermer.checked) {
-        modale.style.display = "none"
-    }
-})
-
-// modale.addEventListener("popup", popup)
+const buttons = document.querySelectorAll(".btn");
+const modales = document.querySelectorAll(".modale");
+const closeButtons = document.querySelectorAll(".close");
 
 
-// Fonction pour les données json
-function dataJson(params) {
-    
-}
+buttons.forEach((button, index) => {
+        button.addEventListener("click", (e) => {
+                // empêche l'activation du lien "a", et donc le rafraîchissement de la page.
+                e.preventDefault ()
+                // lorsqu'un des boutons est sélectionné la class active est activée "add".
+        modales[index].classList.add("active"); 
+});
+});
+
+
+closeButtons.forEach((closeButton, index) => {
+        closeButton.addEventListener("click", () => {
+                // lorsque le bouton fermer est sélectionné on enlève la class active "remove".
+        modales[index].classList.remove("active"); 
+        
+});});
+
 
