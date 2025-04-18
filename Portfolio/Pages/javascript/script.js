6f92ac892c0d35f00acd0cddee56ce24057a0477
Solène vandercruyssen <solenevandercruyssen2@gmail.com>
2025-04-18 15:55:28 +0200

    dcs: ajout d'une page projects

--- Portfolio/Pages/javascript/script.js
+++ Portfolio/Pages/javascript/script.js
@@ -8,3 +8,12 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+// On passe de 0 à 360° à partir de l'axe Y (de gauche à droite)
+let rotation = 0;
+
+const photo = document.querySelector(".photo");
+photo.addEventListener("click", () => {
+    rotation += 360; 
+    photo.style.transform = `rotateY(${rotation}deg)`; 
+});
